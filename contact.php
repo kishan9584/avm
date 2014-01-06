4d4bacbc52d35f9251238b41f1043defb192d8e8
hari kishan <harikrishna@rkandcompany.co.in>
2014-01-06 17:14:03 +0530

    First commit

---
+++ contact.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>A V Masala</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" href="css/default.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css?v=2.1.4" media="screen" />
+		<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="container">
+			<header>
+				<div class="wrapper">	
+					<div id="logo">
+						<a href="index.html"><img src="images/logo_new.png" alt="logo" /></a>
+					</div>
+					<nav>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About Us</a></li>
+							<li><a href="products.html">Products</a></li>
+							<li><a href="#">Media</a></li>
+							<li><a href="#">Career</a></li>
+							<li class="active"><a href="contact.php">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="clear"></div>
+			</header>
+			<div class="content">
+				<div class="wrapper about">	
+					<div class="left">
+						<div class="left-content">	
+							<h3>Contact Us</h3>
+						</div>
+					</div>
+					<div class="right">
+						<div class="right-content contact">
+							<p>Feel free to visit us or contact us on phone or e-mail.</p>
+							<p>Write to us with your feedback at <a href="mailto:feedback@avmasalafoods.com">feedback@avmasalafoods.com</a>. Your opinion counts! We will be glad to address any suggestion or query you may have. You can get a glimpse of our products at <a href="http://www.avmasalafoods.com">www.avmasalafoods.com.</a></p>
+							<p>Inquisitive about us?Want to learn how to contact? </p>
+							<p>Call us on <strong>080-26678936</strong> or write to us at <a href="mailto:info@avmasalafoods.com">info@avmasalafoods.com</a></p>
+							<h3 class="ideals">Registered Office</h3>
+							<p class="address"><strong>AV Masala Foods</strong><br />#42-D, 17th Main, New Timber Yard Layout<br />Muneshwara Block<br />Bangalore-560026</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<footer>
+				<div class="wrapper">
+					<p>&copy;Copyright 2013 AV Masala</p>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Media</a></li>
+						<li><a class="last" href="#">Contact Enquiry</a></li>
+					</ul>
+					<p>Designed By <a href="http://www.webdesignpis.com/" target="_blank">Prabha Info Solutions</a></p>
+				</div>
+				<div class="clear"></div>
+			</footer>
+		</div>
+		<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+		<script type="text/javascript" src="js/jquery.fancybox.js?v=2.1.4"></script>
+	    <script type="text/javascript">
+	    	$(window).load(function() {
+		        $('#slider').nivoSlider();
+		    });
+	    </script>
+	    <script type="text/javascript">
+		$(".fancybox")
+	    .attr('rel', 'gallery')
+	    .fancybox({
+	        openEffect : 'none',
+	        helpers : {
+	            title : null            
+	        }           
+	    });
+	    </script>
+	</body>
+</html>
